43c2b64726d17d68ba464b8fe6a0388bacad8129
DE SOUSA Elisa <edesousa@normandiewebschool.fr>
2022-10-13 16:38:37 +0200

    Create index.php + form.php + readme

---
+++ view/form.php
@@ -0,0 +1,43 @@
+<div class="wrapper">
+    <h1>Ajout d'un nouvel étudiant</h1>
+    <form action="post">
+        <div>
+            <label for="">Prénom</label>
+            <input type="text">
+        </div>
+        <div>
+            <label for="">Nom</label>
+            <input type="text">
+        </div>
+        <div>
+            <label for="">Email</label>
+            <input type="email">
+        </div>
+        <div>
+            <label for="">Téléphone</label>
+            <input type="text">
+        </div>
+        <div>
+            <label for="">Année</label>
+            <select name="" id="">
+                <option value="">Année 1</option>
+                <option value="">Année 2</option>
+                <option value="">Année 3</option>
+                <option value="">Non renseigné</option>
+            </select>
+        </div>
+        <div>
+            <label for="">Spécialité souhaitée</label>
+            <select name="" id="">
+                <option value="">Marketing</option>
+                <option value="">Développement web</option>
+                <option value="">Communication graphique</option>
+                <option value="">Social media manager</option>
+                <option value="">Non renseigné</option>
+            </select>
+        </div>
+        <div class="contact_submit">
+            <button type="submit" class="btn_submit" id="submit" name="submit">Créer un nouvel étudiant</button>
+        </div>
+    </form>
+</div>
