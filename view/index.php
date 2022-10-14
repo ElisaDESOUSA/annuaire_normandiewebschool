dd48e051df0d32a64282e8159f8405203518231b
DE SOUSA Elisa <edesousa@normandiewebschool.fr>
2022-10-14 12:04:33 +0200

    Add CSS on index.php and form.php and Responsive
    Create a navbar +  responsive

--- view/index.php
+++ view/index.php
@@ -9,6 +9,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-
+    <nav class="navigation_bar">
+        <div class="wrapper">
+            <div class="navigation">
+                <a href="index.php" id="navigation_logo"><img src="images/Logo_nws" class="logo" alt="logo_header"></a>
+                <div class="navigation_deroulante" id="navigation_responsive">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
+                <ul class="navigation_menu">
+                    <li class="navigation_tabs">
+                        <a href="#" class="navigation_link" id="home-page" alt="Vers_La_Page_Accueil">Navigation</a>
+                    </li>
+                    <li class="navigation_onglet">
+                        <a href="#" class="navigation_link" id="portfolio-page" alt="Vers_Le_Portfolio">Navigation</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <script src="view/scripts/script.js"></script>
 </body>
 </html>
